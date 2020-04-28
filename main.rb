a3f27ea151c0f4896b076c241f7c2f5bb3aabfad
xxKeefer <xxkeefer.code@gmail.com>
2020-04-28 18:24:34 +1000

    cleaned up style for snake class added functions: turn, go_(directions)

--- main.rb
+++ main.rb
@@ -5,5 +5,13 @@ require_relative './src/mod/snake.rb'
 set title: 'xxTron'
 
 player = Snake::Player.new
-player.draw
+
+update do
+  on :key_down do |event|
+    player.turn(event.key)
+  end
+  player.update
+  # player.info
+  player.draw
+end
 show # displays the window
--- src/mod/snake.rb
+++ src/mod/snake.rb
@@ -11,10 +11,64 @@ module Snake
       @pos_y = pos_y
       @size = size
       @color = color
+      @speed = @size
+      @spd_x = 0
+      @spd_y = 0
+      @tail = []
     end
 
     def draw
       Square.new(x: @pos_x, y: @pos_y, size: @size, color: @color)
+    end
+
+    def turn(dir)
+      case dir
+      when 'w'
+        go_up
+      when 'a'
+        go_left
+      when 's'
+        go_down
+      when 'd'
+        go_right
+      end
+    end
+
+    def go_up
+      return if @spd_y == 1
+
+      @spd_y = -1
+      @spd_x = 0
+    end
+
+    def go_left
+      return if @spd_x == 1
+
+      @spd_y = 0
+      @spd_x = -1
+    end
+
+    def go_down
+      return if @spd_y == -1
+
+      @spd_y = 1
+      @spd_x = 0
+    end
+
+    def go_right
+      return if @spd_x == -1
+
+      @spd_y = 0
+      @spd_x = 1
+    end
+
+    def update
+      @pos_x += (@spd_x * @speed)
+      @pos_y += (@spd_y * @speed)
+    end
+
+    def info
+      puts "Position: [#{@pos_x},#{@pos_y}] Direction [#{@spd_x},#{@spd_y}]"
     end
   end
 end
