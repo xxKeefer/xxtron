69c974c86d85ce0739aa4a0a18e2a5f3366e7545
xxKeefer <xxkeefer.code@gmail.com>
2020-04-28 19:43:06 +1000

    added collision for player snake

--- main.rb
+++ main.rb
@@ -4,13 +4,14 @@ require_relative './src/mod/req.rb'
 require_relative './src/mod/snake.rb'
 set title: 'xxTron'
 
-player = Snake::Player.new
+player = Snake::Player.new(Window.width, Window.height)
 
 update do
   on :key_down do |event|
     player.turn(event.key)
   end
   player.update
+  clear if player.crashed
   # player.info
   player.draw
 end
--- src/mod/snake.rb
+++ src/mod/snake.rb
@@ -6,15 +6,19 @@ module Snake
   require_relative 'req.rb'
   # this class is for player controlled entities
   class Player
-    def initialize(pos_x = 10, pos_y = 10, size = 5, color = 'purple')
+    attr_reader :crashed
+    def initialize(map_width, map_hieght, pos_x = 10, pos_y = 10, size = 5, color = 'purple')
       @pos_x = pos_x
       @pos_y = pos_y
       @size = size
       @color = color
       @speed = @size
-      @spd_x = 0
+      @spd_x = 1
       @spd_y = 0
       @tail = []
+      @crashed = false
+      @map_width = map_width
+      @map_hieght = map_hieght
     end
 
     def draw
@@ -63,12 +67,25 @@ module Snake
     end
 
     def update
+      detect_crash
+      @tail.push([@pos_x, @pos_y])
       @pos_x += (@spd_x * @speed)
       @pos_y += (@spd_y * @speed)
     end
 
+    def detect_crash
+      crash if @tail.detect { |e| @tail.count(e) > 1 } # hit own tail
+      crash if @pos_x < 0 || @pos_y < 0 # snake outside lower bounds
+      crash if @pos_x > @map_width || @pos_y > @map_hieght
+    end
+
+    def crash
+      @crashed = true
+    end
+
     def info
-      puts "Position: [#{@pos_x},#{@pos_y}] Direction [#{@spd_x},#{@spd_y}]"
+      puts "Crashed? #{@crashed} Position: [#{@pos_x},#{@pos_y}]" \
+           " Direction [#{@spd_x},#{@spd_y}]"
     end
   end
 end
